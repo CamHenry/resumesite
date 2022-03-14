1ffe37cb44cf16c722be00652ce2b53bf388e2fa
Cameron Henry <cameronhenryexe@gmail.com>
2022-03-13 22:04:13 -0500

    Git Test

--- pages/resume1.tsx
+++ pages/resume1.tsx
@@ -102,7 +102,7 @@ const Resume: NextPage = () => {
         </span>
 
         <a
-          className='flex items-center justify-center'
+          className='flex items-center justify-center w-full'
           href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
           target='_blank'
           rel='noopener noreferrer'
